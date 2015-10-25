329538db0e348cb8957c671266eee210850171db
Fulas <raullazaro94@gmail.com>
2015-10-25 21:45:37 +0100

    PT100 simulation

--- BrewControl.ino
+++ BrewControl.ino
@@ -6,7 +6,7 @@ EnergyMonitor emon1;  // Create an instance
 float Tvoltage;
 float Temp;
 //lcd is a 20 chars and 4 line display
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x20, 20, 4);
 //set the lcd address to 0x27 for real
 //set the lcd address to 0x20 for simulation
 
@@ -25,16 +25,16 @@ void loop()
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   delay(500);
   Tvoltage = analogRead(Tinput);
-  if (Tvoltage < 410) {
+  /*if (Tvoltage < 410) {
     lcd.clear();
     lcd.print("Error");
-  }
-  if (Tvoltage > 409) {
+  }*/
+  //if (Tvoltage > 409) {
     Tvoltage = map(Tvoltage, 410, 1023, 0, 614);
     Temp = (150 * Tvoltage) / 614;
     lcd.clear();
     lcd.print(Temp, 2);
-  }
+  //}
   //lcd.print(Irms * 230.0); // Apparent power
   //lcd.println(Irms);  // Irms
 }
