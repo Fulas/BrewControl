71a0c545d77b5110510523064a6193a388976e25
Fulas <raullazaro94@gmail.com>
2015-10-31 20:42:20 +0100

    Proteus

--- BrewControl.ino
+++ BrewControl.ino
@@ -2,8 +2,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <EmonLib.h>  // Include Emon Library
 EnergyMonitor emon1;  // Create an instance
-#define TempWaterVoltInput A1  //Temp Watter
-#define TempOilVoltInput A2  //Temp Oil
+#define TempWaterInput A1  //Temp Watter
+#define TempOilInput A2  //Temp Oil
 //lcd is a 20 chars and 4 line display
 LiquidCrystal_I2C lcd(0x20, 20, 4);
 //set the lcd address to 0x27 for real
@@ -14,8 +14,8 @@ void setup()
   Wire.begin();
   lcd.init();
   lcd.backlight();
-  pinMode(TempWaterVoltInput, INPUT);
-  pinMode(TempOilVoltInput, INPUT);
+  pinMode(TempWaterInput, INPUT);
+  pinMode(TempOilInput, INPUT);
   emon1.current(1, 111.1); // Current: input pin, calibration.
 }
 
@@ -31,22 +31,20 @@ void loop()
 
 void temp()
 {
-  float TemWaterVolt;
-  float TempOilVolt;
   float TempWater;
   float TempOil;
-  TemWaterVolt = analogRead(TempWaterVoltInput);
-  TempOilVolt = analogRead(TempOilVoltInput);
+  TempWater = analogRead(TempWaterInput);
+  TempOil = analogRead(TempOilInput);
   //Temp Water
   //T=50*V-100
   //0C-->2V-->409
   //150C-->5V-->1023
   lcd.setCursor(0, 0);
-  if (TemWaterVolt < 410) {
+  if (TempWater < 410) {
     lcd.print("Error");
   }
-  if (TemWaterVolt > 409) {
-    TempWater = map(TemWaterVolt, 410, 1023, 0, 150);
+  if (TempWater > 409) {
+    TempWater = map(TempWater, 410, 1023, 0, 150);
     lcd.print(TempWater, 2);
   }
   //Temp Oil
@@ -54,11 +52,11 @@ void temp()
   //0C-->1V-->204
   //400C-->5V-->1023
   lcd.setCursor(7, 0);
-  if (TempOilVolt < 205) {
+  if (TempOil < 205) {
     lcd.print("Error");
   }
-  if (TempOilVolt > 204) {
-    TempOil = map(TempOilVolt, 205, 1023, 0, 400);
+  if (TempOil > 204) {
+    TempOil = map(TempOil, 205, 1023, 0, 400);
     lcd.print(TempOil, 2);
   }
 }
